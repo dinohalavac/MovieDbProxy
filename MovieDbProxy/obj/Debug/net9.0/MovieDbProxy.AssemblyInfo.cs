9b8b124d7fa625063190c052952ea0535806e888
dinohalavac <dino_halavac@hotmail.com>
2025-05-17 17:20:38 +0200

    Update

--- MovieDbProxy/obj/Debug/net9.0/MovieDbProxy.AssemblyInfo.cs
+++ MovieDbProxy/obj/Debug/net9.0/MovieDbProxy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieDbProxy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+745bfb9fbece509dd0fa94fab11d2ea3d651d346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c002de4567646b99b204e01905a92435b81a372")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieDbProxy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieDbProxy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

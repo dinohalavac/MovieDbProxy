0697d9bd0d25e1aea71ed4e74983e9c1f6d23743
dinohalavac <dino_halavac@hotmail.com>
2025-05-17 20:09:12 +0200

    add Key

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
+using MovieDbProxy.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular4200", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHttpClient();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Services.Configure<MovieDBSettings>(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
